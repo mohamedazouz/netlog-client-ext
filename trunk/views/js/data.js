44a2c7138f3d9e7d0f772146db33df84ee2211ca
mohamedaliazouz <mohamedaliazouz@58a2d043-760a-d76c-89ee-ed910d788c2f>
2011-10-09 15:21:32 +0000

    adding new changes on code

--- trunk/views/js/data.js
+++ trunk/views/js/data.js
@@ -1,7 +1,7 @@
 NetLogStaticData=function(){
     var netlogStaticData={
-        //baseURL:"http://localhost/oldProjects/netlog_proxy",
-        baseURL:"http://azouz.activedd.com/netlog_proxy/",
+        baseURL:"http://localhost/oldProjects/netlog_proxy",
+        //baseURL:"http://azouz.activedd.com/netlog_proxy",
         loginAuthorizeURL:"/external.php",
         gettokenURL:"/getAuthkey.php",
         dofunctionURL:"/doFunction.php"
