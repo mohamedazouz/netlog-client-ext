bfe7d9910087a6e5dcf75c762fa1894d66381f80
mohamedaliazouz <mohamedaliazouz@58a2d043-760a-d76c-89ee-ed910d788c2f>
2011-10-11 21:58:42 +0000

    add new proxy

--- trunk/views/js/data.js
+++ trunk/views/js/data.js
@@ -1,7 +1,9 @@
 NetLogStaticData=function(){
     var netlogStaticData={
         //baseURL:"http://localhost/oldProjects/netlog_proxy",
-        baseURL:"http://azouz.activedd.com/netlog_proxy",
+        //baseURL:"http://azouz.activedd.com/netlog_proxy",
+        baseURL:" http://netlog.activedd.co.cc",
+        //baseURL:"http://localhost/",
         loginAuthorizeURL:"/external.php",
         gettokenURL:"/getAuthkey.php",
         dofunctionURL:"/doFunction.php"
