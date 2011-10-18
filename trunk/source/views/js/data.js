43a9245c201358e267f08cb29aed673c91949500
mohamedaliazouz <mohamedaliazouz@58a2d043-760a-d76c-89ee-ed910d788c2f>
2011-10-18 11:59:41 +0000

    fixing allow and deny application

--- trunk/source/views/js/data.js
+++ trunk/source/views/js/data.js
@@ -2,7 +2,7 @@ NetLogStaticData=function(){
     var netlogStaticData={
         //baseURL:"http://localhost/oldProjects/netlog_proxy",
         //baseURL:"http://azouz.activedd.com/netlog_proxy",
-        baseURL:" http://netlog.activedd.co.cc",
+        baseURL:"http://extensions.activedd.com/netlog_proxy",
         //baseURL:"http://localhost/",
         loginAuthorizeURL:"/external.php",
         gettokenURL:"/getAuthkey.php",
